55962aa7ca341284f694306378c32a99c9626fb1
duskull <126384272+drmoa@users.noreply.github.com>
2026-04-14 08:24:45 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::unordered_map<char, int> freq;
+        for (char c : s) { freq[c]++; }
+        for (char c : t) { freq[c]--; }
+
+        for (auto& [ch, count] : freq) {
+            if (count != 0) { return false; }
+        }
+        return true;
+    }
+};
